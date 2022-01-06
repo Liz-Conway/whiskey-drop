9de06cff57b8242054e5c3a53647634cd61f1edc
Fintan <fintan@waycontech.com>
2022-01-06 21:09:48 +0000

    Whiskey Drop

---
+++ sass/sass-setup.sh
@@ -0,0 +1,12 @@
+touch main.sass
+mkdir abstracts base components layout pages themes vendors
+cd abstracts
+touch _colours.sass _functions.sass _mixins.sass _variables.sass _media-queries.sass
+cd ../base/
+touch _animations.sass _base.sass _typography.sass _utilities.sass
+cd ../components/
+touch _button.sass
+cd ../layout/
+touch _grid.sass _header.sass 
+cd ../pages/
+touch _home.sass
